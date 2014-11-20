3dd2cb5610fc58cfe835f67ecba4a11c281afe46
Denis Gilè <denis@gfaktor.com>
2014-11-20 16:29:04 +0100

    titano

---
+++ testin/titanio/form-select.js
@@ -0,0 +1,67 @@
+function load_regioni() {
+  $.ajax({
+    type: "get",
+    url: 'italia.json',
+    dataType: "json",
+    success: function (data) {
+      $('#regione').empty().append('<option value="">Seleziona Regione</option>').removeAttr('disabled');
+      $.each(data.nome, function (index, item) {
+        var n = item.nome;
+        $('#regione').append('<option value="' + n +'">' + n + '</option>');
+      });
+    }
+  });
+}
+
+function load_province(regione) {
+  $.ajax({
+    type: "post",
+    url: 'italia.json',
+    dataType: "json",
+    success: function (data) {
+      $('#comune' + cat).empty().append('<option value="">Seleziona Provincia</option>').removeAttr('disabled');
+      $.each(data.comuni, function (index, item) {
+        var label = item.DESCRIZIONE;
+        var id = item.CODICE;
+        $('#comune' + cat).append('<option value="' + id + '">' + label + '</option>');
+      });
+    }
+  });
+}
+
+//READY
+$(function () {
+
+  $('input[name=localizzazione]:radio').change(function(){
+      if($(this).val()=='locali'){
+        load_regioni();
+      }else if ($(this).val()=='nazionali'){
+        $('#regione').attr('disabled','disabled');
+        $('#provincia').attr('disabled','disabled');
+      }
+  });
+
+  $('#regione').change(function(){
+      if($(this).val()!='0'){
+        load_province();
+      }else{ 
+        $('#provincia').attr('disabled','disabled');
+      }
+  });
+
+  $('#messaggio').keyup(function(){
+      if(this.value.match(/[^a-zA-Z0-9,.:; ]/g)) this.value = this.value.replace(/[^a-zA-Z0-9,.:; ]/g, '');
+  });
+
+
+  $('.regione').change(function() {
+    var cat = $(this).attr('rel');
+    var provincia = $('option:selected', this).attr('label');
+    checkprovince(cat, provincia);
+  });
+
+  $('#form').submit(function(event){
+    event.preventDefault();
+  });
+
+});
