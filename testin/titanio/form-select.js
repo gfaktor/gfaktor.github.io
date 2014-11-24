81e8f3e469198eb71d14a379d5ef491671dd4dba
Denis Gilè <denis@gfaktor.com>
2014-11-24 12:37:33 +0100

    next

--- testin/titanio/form-select.js
+++ testin/titanio/form-select.js
@@ -1,46 +1,53 @@
-function load_selects() {
+var italia;
+
+function load_json(){
   $.ajax({
     type: "get",
     url: 'italia.json',
     dataType: "json",
-    success: function (data) {
-
-        $('#regione').empty().append('<option value="0">Seleziona Regione</option>').removeAttr('disabled');
-        
-        $.each(data.regioni, function (key, val) {
-            var n = val.nome;
-            $('#regione').append('<option value="' + n +'">' + n + '</option>');
-        });
-        
-        $('#regione').change(function(){
-            if($(this).val()!='0'){
-                $('#provincia').removeAttr('disabled');
-            }
-        });
-        
+    success: function (data){
+        var italia = data.regioni;
     }
-  });
 }
+
+
+function load_selects() {
+      console.log(italia);
+    /*  
+    $('#regione').empty().append('<option value="0">Seleziona Regione</option>').removeAttr('disabled');
+        
+    $.each(italia, function (key, val) {
+        var regione = val.nome;
+        $('#regione').append('<option value="' + regione +'">' + regione + '</option>');
+    });
+
+    $('#regione').change(function(){
+        if($(this).val()!='0')
+            $('#provincia').removeAttr('disabled');
+    });
+    */  
+}
+
+
 
 //READY
 $(function () {
-  $.ajaxSetup({ cache: true });    
     
-  $('input[name=localizzazione]:radio').change(function(){
-      if($(this).val()=='locali'){
-        load_selects();
-      }else if ($(this).val()=='nazionali'){
-        $('#regione').empty().append('<option value="">...</option>').attr('disabled','disabled');
-        $('#provincia').empty().append('<option value="">...</option>').attr('disabled','disabled');  
-      }
-  });
+    load_json();
+    
+    $('input[name=localizzazione]:radio').change(function(){
+        if($(this).val()=='locali')
+            load_selects();
+        else if ($(this).val()=='nazionali')
+            $('#regione, #provincia').empty().append('<option value="">...</option>').attr('disabled','disabled');
+    });
 
-  $('#messaggio').keyup(function(){
-      if(this.value.match(/[^a-zA-Z0-9,.:; ]/g)) this.value = this.value.replace(/[^a-zA-Z0-9,.:; ]/g, '');
-  });
+    $('#messaggio').keyup(function(){
+        if(this.value.match(/[^a-zA-Z0-9,.:; ]/g)) this.value = this.value.replace(/[^a-zA-Z0-9,.:; ]/g, '');
+    });
 
-  $('#form').submit(function(event){
-    event.preventDefault();
-  });
+    $('#form').submit(function(event){
+        event.preventDefault();
+    });
 
 });
